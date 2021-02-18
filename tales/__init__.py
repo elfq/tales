eb3c7b879fff2111bf7de15f80497ece297b6297
elf <shadenitey@Gmail.com>
2021-02-18 08:57:36 +0000

    fix imports

--- tales/__init__.py
+++ tales/__init__.py
@@ -1,3 +1,3 @@
-from .api_key import * 
+from .auth import * 
 from .bedwars import * 
 from .blitz import * 
--- tales/bedwars.py
+++ tales/bedwars.py
@@ -1,5 +1,5 @@
 import requests
-from .api_key import api
+from .auth import api
 
 class bedwars:
   """
