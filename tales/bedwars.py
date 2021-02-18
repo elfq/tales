99f993181472e3abf00381804e3f1f492b7ed099
elf <shadenitey@Gmail.com>
2021-02-18 09:00:59 +0000

    indents

--- tales/bedwars.py
+++ tales/bedwars.py
@@ -6,28 +6,28 @@ class bedwars:
   Stat tracking for the Bedwars Minigame.
   """
 
-    def wins(username):
+  def wins(username):
       """
       :bedwars.wins: Track wins amounts.
       """
         r = requests.get(f"https://api.hypixel.net/player?key={api.key}&name={username}")
         return r.json()["player"]["achievements"]["bedwars_wins"]
 
-    def beds(username):
+  def beds(username):
       """
       :bedwars.beds: Track amount of beds destroyed.
       """
         r = requests.get(f"https://api.hypixel.net/player?key={api.key}&name={username}")
         return r.json()["player"]["achievements"]["bedwars_beds"]
 
-    def level(username):
+  def level(username):
       """
       :bedwars.level: Track level.
       """
         r = requests.get(f"https://api.hypixel.net/player?key={api.key}&name={username}")
         return r.json()["player"]["achievements"]["bedwars_level"]
 
-    def kills(username):
+  def kills(username):
       """
       :blitz.kills: Track kills amounts.
       """
--- tales/blitz.py
+++ tales/blitz.py
@@ -6,21 +6,21 @@ class blitz:
   Stat tracking for the Blitz minigame.
   """
 
-    def wins(username):
+  def wins(username):
       """
       :blitz.wins: Track wins amounts.
       """
         r = requests.get(f"https://api.hypixel.net/player?key={api.key}&name={username}")
         return r.json()["player"]["achievements"]["blitz_wins"]
 
-    def kills(username):
+  def kills(username):
       """
       :blitz.kills: Track kill amount.
       """
         r = requests.get(f"https://api.hypixel.net/player?key={api.key}&name={username}")
         return r.json()["player"]["achievements"]["blitz_kills"]
 
-    def coins(username):
+  def coins(username):
       """
       :blitz.coins: Track coins amounts.
       """
